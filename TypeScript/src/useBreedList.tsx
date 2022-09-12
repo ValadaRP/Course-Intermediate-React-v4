74de17e26f049678573a0df69ee27ed5d8cff6b9
ValadaRP <79703007+ValadaRP@users.noreply.github.com>
2022-09-12 23:41:24 +0200

    useBreedList conversion to tsx

--- TypeScript/src/useBreedList.tsx
+++ TypeScript/src/useBreedList.tsx
@@ -1,10 +1,16 @@
 import { useState, useEffect } from "react";
+import { Animal, BreedListAPIResponse } from "./APIResponsesTypes";
 
-const localCache = {};
+const localCache:{
+  [index: string]: string[]
+} 
+= {};
 
-export default function useBreedList(animal) {
-  const [breedList, setBreedList] = useState([]);
-  const [status, setStatus] = useState("unloaded");
+type Status = "unloaded" | "loading" | "loaded";
+
+export default function useBreedList(animal: Animal) {
+  const [breedList, setBreedList] = useState([] as string[]);
+  const [status, setStatus] = useState("unloaded" as Status);
 
   useEffect(() => {
     if (!animal) {
